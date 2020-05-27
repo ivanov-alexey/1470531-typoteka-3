a560b66b018239a42e1db5b19c218e96688ea5e9
Alexey Ivanov <4985900@gmail.com>
2020-05-27 16:26:23 +0300

    feat: add pug templates

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,61 +1,44 @@
 'use strict';
 
-const http = require(`http`);
+const express = require(`express`);
 const fs = require(`fs`).promises;
 const {FILE_NAME, DEFAULT_API_PORT, HttpCode, Message} = require(`../../constants`);
 const {logger} = require(`../../utils`);
 
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-    <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>With love from Node</title>
-      </head>
-      <body>${message}</body>
-    </html>`.trim();
+const app = express();
 
-  res.statusCode = statusCode;
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
+app.use(express.json());
 
-  res.end(template);
-};
+app.get(`/posts`, async (req, res) => {
+  try {
+    const fileContent = await fs.readFile(FILE_NAME);
+    const mocks = JSON.parse(fileContent);
 
-const onClientConnect = async (req, res) => {
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await fs.readFile(FILE_NAME);
-        const mocks = JSON.parse(fileContent);
-        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-
-        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
-      } catch (err) {
-        sendResponse(res, HttpCode.NOT_FOUND, Message.notFound);
-      }
-
-      return;
-    default:
-      sendResponse(res, HttpCode.NOT_FOUND, Message.notFound);
-      return;
+    res.json(mocks);
+  } catch (err) {
+    res.status(HttpCode.INTERNAL_SERVER_ERROR).send(err);
   }
-};
+});
+
+app.use((req, res) => res
+  .status(HttpCode.NOT_FOUND)
+  .send(`Not found`));
 
 module.exports = {
   name: `--server`,
   run(args) {
     const port = Number.parseInt(args, 10) || DEFAULT_API_PORT;
 
-    http.createServer(onClientConnect)
-      .listen(port)
-      .on(`listening`, (err) => {
+    try {
+      app.listen(DEFAULT_API_PORT, (err) => {
         if (err) {
           return logger.error(err);
         }
 
         return logger.success(Message.listenOnPort(port));
       });
+    } catch (err) {
+      logger.error(err);
+    }
   }
 };
