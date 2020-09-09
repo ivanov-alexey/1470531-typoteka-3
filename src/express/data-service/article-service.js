563e48ccdc005d9c7d084cdff2def9b08008855b
Ivanov Alexey <4985900@gmail.com>
2020-09-09 10:43:37 +0300

    feat: render data for main route

---
+++ src/express/data-service/article-service.js
@@ -0,0 +1,99 @@
+'use strict';
+
+const axios = require(`axios`);
+const {API_URL, TimeInMilliseconds} = require(`../../constants`);
+
+const instance = axios.create({
+  baseURL: API_URL,
+  timeout: TimeInMilliseconds * 30
+});
+
+class ArticleService {
+  constructor(article) {
+    this.article = article;
+  }
+
+  static async getAllArticles() {
+    try {
+      const response = await instance.get(`/articles`);
+
+      return response.data;
+    } catch (error) {
+      console.error(error);
+
+      return [];
+    }
+  }
+
+  static async getCategories() {
+    try {
+      const response = await instance.get(`/categories`);
+
+      return response.data;
+    } catch (err) {
+      console.error(err);
+
+      return [];
+    }
+  }
+
+  static async getCategoriesWithArticlesCounter() {
+    try {
+      const articles = await ArticleService.getAllArticles();
+      const categoriesInArticles = articles.map((article) => article.category).flat();
+
+      return categoriesInArticles.map((category) => ({
+        name: category,
+        count: categoriesInArticles.filter((item) => item === category).length
+      }));
+    } catch (err) {
+      console.error(err);
+
+      return [];
+    }
+  }
+
+  static async getMostDiscussed() {
+    try {
+      const articles = await ArticleService.getAllArticles();
+
+      return articles
+        .sort((prev, next) => next.comments.length - prev.comments.length)
+        .slice(0, 4)
+        .map((article) => {
+          const croppedAnnounce = article.announce.slice(0, 100).trim();
+          const announce = croppedAnnounce
+            .slice(0, croppedAnnounce.endsWith(`,`) ? croppedAnnounce.length - 1 : croppedAnnounce.length) + `...`;
+
+          return {
+            id: article.id,
+            announce,
+            commentsLength: article.comments.length
+          };
+        });
+    } catch (err) {
+      console.error(err);
+
+      return [];
+    }
+  }
+
+  static async getLastComments() {
+    try {
+      const articles = await ArticleService.getAllArticles();
+
+      return articles
+        .map((article) => article.comments)
+        .flat()
+        .sort((prev, next) => next.date - prev.date)
+        .slice(0, 4);
+
+    } catch (err) {
+      console.error(err);
+
+      return [];
+    }
+  }
+}
+
+module.exports = ArticleService;
--- src/express/routes/main-route.js
+++ src/express/routes/main-route.js
@@ -1,9 +1,23 @@
 'use strict';
 
 const {Router} = require(`express`);
+const ArticleService = require(`../data-service/article-service`);
 
 const mainRoute = new Router();
 
-mainRoute.get(`/`, (req, res) => res.render(`main`));
+mainRoute.get(`/`, async (req, res) => {
+  const allArticles = await ArticleService.getAllArticles();
+  const categories = await ArticleService.getCategoriesWithArticlesCounter();
+  const popularArticles = await ArticleService.getMostDiscussed();
+  const lastComments = await ArticleService.getLastComments();
+  const articles = allArticles.slice(0, 8);
+
+  res.render(`main`, {
+    articles,
+    categories,
+    popularArticles,
+    lastComments
+  });
+});
 
 module.exports = mainRoute;
