66eb51b233e73ece541f5dcaee8eb4d7c776bc4f
Ivanov Alexey <4985900@gmail.com>
2020-09-10 08:09:06 +0300

    feat: render data for /categories route

--- src/express/routes/categories-routes.js
+++ src/express/routes/categories-routes.js
@@ -1,9 +1,16 @@
 'use strict';
 
 const {Router} = require(`express`);
+const ArticleService = require(`../data-service/article-service`);
 
 const categoriesRoutes = new Router();
 
-categoriesRoutes.get(`/`, (req, res) => res.render(`my/all-categories`));
+categoriesRoutes.get(`/`, async (req, res) => {
+  const categories = await ArticleService.getCategories();
+
+  res.render(`my/all-categories`, {
+    categories
+  });
+});
 
 module.exports = categoriesRoutes;
