9e10f5095bc340d366d7dc076e6634e31ba5e137
Ivanov Alexey <4985900@gmail.com>
2020-09-14 13:19:15 +0300

    fix: change TIMESTAMP to timestamptz. Add CASCADE option for drop tables

--- schema.sql
+++ schema.sql
@@ -8,11 +8,11 @@ CREATE DATABASE typoteka
     TEMPLATE = template0
     CONNECTION LIMIT = -1;
 
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS articles;
-DROP TABLE IF EXISTS categories;
-DROP TABLE IF EXISTS comments;
-DROP TABLE IF EXISTS categories_articles;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS articles CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS comments CASCADE;
+DROP TABLE IF EXISTS categories_articles CASCADE;
 
 CREATE TABLE users
 (
@@ -22,7 +22,7 @@ CREATE TABLE users
     firstname TEXT NOT NULL,
     lastname TEXT NOT NULL,
     password TEXT NOT NULL ,
-    created_at TIMESTAMP NOT NULL
+    created_at timestamptz NOT NULL
 );
 
 CREATE TABLE articles
@@ -32,38 +32,38 @@ CREATE TABLE articles
     full_text TEXT NOT NULL,
     picture TEXT,
     title TEXT NOT NULL,
-    publication_date TIMESTAMP NOT NULL,
-    created_at TIMESTAMP NOT NULL
+    publication_date timestamptz NOT NULL,
+    created_at timestamptz NOT NULL
 );
 
 CREATE TABLE categories
 (
     id INTEGER PRIMARY KEY,
     title TEXT,
-    created_at TIMESTAMP
+    created_at timestamptz
 );
 
 CREATE TABLE comments
 (
     id INTEGER PRIMARY KEY,
     text TEXT,
-    publication_date TIMESTAMP,
-    created_at TIMESTAMP,
+    publication_date timestamptz,
+    created_at timestamptz,
     user_id INTEGER,
     article_id INTEGER,
     FOREIGN KEY (user_id) REFERENCES users (id)
         ON DELETE SET NULL
         ON UPDATE SET NULL,
-    FOREIGN KEY (article_id) REFERENCES articles(id)
+    FOREIGN KEY (article_id) REFERENCES articles (id)
         ON DELETE SET NULL
         ON UPDATE SET NULL
 );
 
 CREATE TABLE categories_articles
 (
+    id INTEGER PRIMARY KEY,
     category_id INTEGER NOT NULL,
     article_id INTEGER NOT NULL,
-    CONSTRAINT categories_articles_pk PRIMARY KEY (category_id, article_id),
     FOREIGN KEY (category_id) REFERENCES categories (id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
