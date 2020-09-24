23ffc9c90994d2fea388306641d0da2c7ed9c926
Ivanov Alexey <4985900@gmail.com>
2020-09-24 16:20:53 +0300

    fix: remove cyrillic aliases, add aliases for tables

--- db/queries.sql
+++ db/queries.sql
@@ -1,124 +1,115 @@
-/* done Получить список всех категорий
-   (идентификатор, наименование категории); */
-SELECT id    AS "Идентификатор",
-       title AS "Наименование категории"
+/* done Получить список всех категорий (идентификатор, наименование категории); */
+SELECT id    AS "identifier",
+       title AS "category_name"
 FROM categories;
 
 
-/* Получить список категорий для которых создана
-   минимум одна публикация
+/* Получить список категорий для которых создана минимум одна публикация
    (идентификатор, наименование категории); */
-SELECT categories.id    AS "Идентификатор",
-       categories.title AS "Наименование категории"
-FROM categories_articles
-       INNER JOIN categories on categories_articles.category_id = categories.id
-GROUP BY categories.id
-HAVING count(categories.id) > 0;
+SELECT c.id    AS "identifier",
+       c.title AS "category_name"
+FROM categories_articles AS ca
+         INNER JOIN categories AS c on ca.category_id = c.id
+GROUP BY c.id
+HAVING count(c.id) > 0;
 
 
-/* Получить список категорий с количеством публикаций
-   (идентификатор, наименование категории,
-    количество публикаций в категории); */
-SELECT categories.id                          AS "Идентификатор",
-       categories.title                       AS "Наименование категории",
-       count(categories_articles.category_id) AS "Количество публикаций в категории"
-FROM categories
-       LEFT JOIN categories_articles ON categories_articles.category_id = categories.id
-GROUP BY categories.id;
+/* Получить список категорий с количеством публикаций (идентификатор,
+   наименование категории, количество публикаций в категории); */
+SELECT c.id                  AS "identifier",
+       c.title               AS "category_name",
+       count(ca.category_id) AS "amount_of_publications"
+FROM categories AS c
+         LEFT JOIN categories_articles AS ca ON ca.category_id = c.id
+GROUP BY c.id;
 
 
-/* Получить список публикаций
-   (идентификатор публикации, заголовок публикации,
-    анонс публикации, дата публикации, имя и фамилия автора,
-    контактный email, количество комментариев,
-    наименование категорий). Сначала свежие публикации; */
-SELECT articles.id                                    AS "идентификатор публикации",
-       articles.title                                 AS "заголовок публикации",
-       articles.announce                              AS "анонс публикации",
-       articles.publication_date                      AS "дата публикации",
-       concat(users.firstname, ' ', users.lastname)   AS "имя и фамилия автора",
-       users.email                                    AS "контактный email",
-       count(comments.article_id)                     AS "количество комментариев",
+/* Получить список публикаций (идентификатор публикации, заголовок публикации,
+    анонс публикации, дата публикации, имя и фамилия автора, контактный email,
+    количество комментариев, наименование категорий). Сначала свежие публикации; */
+SELECT a.id                                 AS "publication_id",
+       a.title                              AS "publication_title",
+       a.announce                           AS "announcement",
+       a.full_text                          AS "full_text",
+       a.publication_date                   AS "publication_date",
+       a.picture                            AS "img_path",
+       concat(u.firstname, ' ', u.lastname) AS "author",
+       u.email                              AS "email",
+       count(com.article_id)                AS "amount_of_comments",
        (
-         SELECT string_agg(categories.title, ', ') AS "наименование категорий"
-         FROM categories_articles
-                LEFT JOIN categories
-                          ON categories_articles.category_id = categories.id
-                            AND categories_articles.article_id = articles.id
+           SELECT string_agg(c.title, ', ') AS "categories_name"
+           FROM categories_articles AS ca
+                    LEFT JOIN categories AS c ON ca.category_id = c.id
+               AND ca.article_id = a.id
        )
-FROM articles
-       INNER JOIN users
-                  ON articles.user_id = users.id
-       INNER JOIN comments
-                  ON articles.id = comments.article_id
-GROUP BY articles.id,
-         articles.publication_date,
-         users.firstname,
-         users.lastname,
-         users.email
-ORDER BY articles.publication_date DESC;
+FROM articles AS a
+         INNER JOIN users AS u
+                    ON a.user_id = u.id
+         INNER JOIN comments AS com ON a.id = com.article_id
+WHERE a.id = 1
+GROUP BY a.id, u.firstname, u.lastname, u, email;
 
 
-/* Получить полную информацию определённой публикации
-   (идентификатор публикации, заголовок публикации, анонс,
-    полный текст публикации, дата публикации, путь к изображению,
-    имя и фамилия автора, контактный email, количество комментариев,
-    наименование категорий); */
-SELECT articles.id                                    AS "идентификатор публикации",
-       articles.title                                 AS "заголовок публикации",
-       articles.announce                              AS "анонс",
-       articles.full_text                             AS "полный текст публикации",
-       articles.publication_date                      AS "дата публикации",
-       articles.picture                               AS "путь к изображению",
-       concat(users.firstname, ' ', users.lastname)   AS "имя и фамилия автора",
-       users.email                                    AS "контактный email",
-       count(comments.article_id)                     AS "Количество комментариев",
+/* Получить полную информацию определённой публикации (идентификатор публикации,
+    заголовок публикации, анонс, полный текст публикации, дата публикации,
+    путь к изображению, имя и фамилия автора, контактный email,
+    количество комментариев, наименование категорий); */
+SELECT a.id                                 AS "publication_id",
+       a.title                              AS "publication title",
+       a.announce                           AS "announcement",
+       a.full_text                          AS "full_text",
+       a.publication_date                   AS "publication_date",
+       concat(u.firstname, ' ', u.lastname) AS "author",
+       u.email                              AS "email",
+       count(com.article_id)                AS "amount_of_comments",
        (
-         SELECT
-           string_agg(categories.title, ', ')          AS "наименование категорий"
-         FROM categories_articles
-                LEFT JOIN categories ON categories_articles.category_id = categories.id
-           AND categories_articles.article_id = articles.id
+           SELECT string_agg(c.title, ', ') AS "categories_name"
+           FROM categories_articles AS ca
+                    LEFT JOIN categories AS c
+                              ON ca.category_id = c.id
+                                  AND ca.article_id = a.id
        )
-FROM articles
-       INNER JOIN users
-                  ON articles.user_id = users.id
-       INNER JOIN comments ON articles.id = comments.article_id
-WHERE articles.id = 1
-GROUP BY articles.id, users.firstname, users.lastname, users, email;
+FROM articles AS a
+         INNER JOIN users AS u
+                    ON a.user_id = u.id
+         INNER JOIN comments AS com
+                    ON a.id = com.article_id
+GROUP BY a.id,
+         a.publication_date,
+         u.firstname,
+         u.lastname,
+         u.email
+ORDER BY a.publication_date DESC;
 
 
-/* Получить список из 5 свежих комментариев
-   (идентификатор комментария, идентификатор публикации,
-    имя и фамилия автора, текст комментария); */
-SELECT comments.id                                    AS "Идентификатор комментария",
-       comments.article_id                            AS "Идентификатор публикации",
-       concat(users.firstname, ' ', users.lastname)   AS "Имя и фамилия автора",
-       comments.text                                  AS "Текст комментария"
-FROM comments
-       INNER JOIN users
-                  ON comments.user_id = users.id
+/* Получить список из 5 свежих комментариев (идентификатор комментария,
+   идентификатор публикации, имя и фамилия автора, текст комментария); */
+SELECT c.id                                 AS "comment_id",
+       c.article_id                         AS "publication_id",
+       concat(u.firstname, ' ', u.lastname) AS "author",
+       c.text                               AS "comment_text"
+FROM comments AS c
+         INNER JOIN users AS u
+                    ON c.user_id = u.id
 ORDER BY publication_date DESC
 LIMIT 5;
 
 
-/* Получить список комментариев для определённой публикации
-   (идентификатор комментария, идентификатор публикации,
-    имя и фамилия автора, текст комментария).
+/* Получить список комментариев для определённой публикации (идентификатор комментария,
+    идентификатор публикации, имя и фамилия автора, текст комментария).
     Сначала новые комментарии; */
-SELECT comments.id                                    AS "Идентификатор комментария",
-       comments.article_id                            AS "Идентификатор публикации",
-       concat(users.firstname, ' ', users.lastname)   AS "Имя и фамилия автора",
-       comments.text                                  AS "Текст комментария"
-FROM comments
-       INNER JOIN users
-                  ON comments.user_id = users.id
+SELECT c.id                                 AS "comment_id",
+       c.article_id                         AS "publication_id",
+       concat(u.firstname, ' ', u.lastname) AS "author",
+       c.text                               AS "comment_text"
+FROM comments AS c
+         INNER JOIN users AS u
+                    ON c.user_id = u.id
 WHERE article_id = 3
 ORDER BY publication_date DESC;
 
 
-/* Обновить заголовок определённой публикации на
-   «Как я встретил Новый год»; */
+/* Обновить заголовок определённой публикации на «Как я встретил Новый год»; */
 UPDATE articles
 set title = 'Как я встретил Новый год'
 WHERE articles.id = 1;
