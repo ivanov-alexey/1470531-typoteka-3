1b182366658ca0ae5401a83195c9a65cf9815b51
Ivanov Alexey <4985900@gmail.com>
2020-10-01 17:21:27 +0300

    feat: update comments api service for working with database

--- src/service/data-service/comment.js
+++ src/service/data-service/comment.js
@@ -1,36 +1,62 @@
 'use strict';
 
-const {nanoid} = require(`nanoid`);
-const {MAX_ID_LENGTH} = require(`../../constants`);
+const {db: {Comment}} = require(`../db/connect`);
 
 class CommentService {
-  create(article, comment) {
-    const newComment = {
-      comment,
-      id: nanoid(MAX_ID_LENGTH)
-    };
-
-    article.comments.push(newComment);
-
-    return comment;
-  }
-
-  drop(article, commentId) {
-    const dropComment = article.comments
-      .find((item) => item.id === commentId);
-
-    if (!dropComment) {
-      return null;
+  async create(userId, text) {
+    try {
+      return await Comment.create({
+        text,
+        'user_id': userId
+      });
+    } catch (err) {
+      return console.error(err);
     }
-
-    article.comments = article.comments
-      .filter((item) => item.id !== commentId);
-
-    return dropComment;
   }
 
-  findAll(article) {
-    return article.comments;
+  async drop(commentId) {
+    try {
+      const comment = await Comment.findByPk(commentId, {raw: true});
+
+      if (!comment) {
+        return null;
+      }
+
+      await Comment.destroy({
+        where: {
+          id: commentId
+        }
+      });
+
+      return comment;
+    } catch (err) {
+      return console.error(err);
+    }
+  }
+
+  async findByArticleId(articleId) {
+    try {
+      const comments = await Comment.findAll({
+        where: {
+          "article_id": articleId
+        },
+        include: `user`,
+        order: [
+          [`created_at`, `DESC`]
+        ],
+        raw: true
+      });
+
+      return comments.map((comment) => ({
+        id: comment.id,
+        author: `${comment[`user.firstname`]} ${comment[`user.lastname`]}`,
+        avatar: comment[`user.avatar`],
+        createdAt: comment[`created_at`],
+        text: comment.text
+      }));
+    } catch (err) {
+      return console.error(err);
+    }
   }
 }
 
