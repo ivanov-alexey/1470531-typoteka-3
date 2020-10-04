83d588f329e728f112f65566f4586e9e27a3d1d5
Ivanov Alexey <4985900@gmail.com>
2020-10-04 11:35:00 +0300

    feat: update comments api service for working with database, add data validation

--- src/service/middlewares/article-validator.js
+++ src/service/middlewares/article-validator.js
@@ -1,17 +1,54 @@
 'use strict';
 
-const {HttpCode} = require(`../../constants`);
+const {HttpCode, TextRestriction} = require(`../../constants`);
 
 const articleKeys = [`category`, `announce`, `fullText`, `title`];
 
 module.exports = (req, res, next) => {
-  const comment = req.body;
-  const keys = Object.keys(comment);
+  const article = req.body;
+  const keys = Object.keys(article);
   const keysExists = articleKeys.every((key) => keys.includes(key));
 
   if (!keysExists) {
-    res.status(HttpCode.BAD_REQUEST)
+    res
+      .status(HttpCode.BAD_REQUEST)
       .send(`Bad request`);
+  }
+
+  if (article.title && article.title.length < TextRestriction.shortMin) {
+    res
+      .status(HttpCode.BAD_REQUEST)
+      .send(`Title too short`);
+  }
+
+  if (article.title && article.title.length < TextRestriction.shortMax) {
+    res
+      .status(HttpCode.BAD_REQUEST)
+      .send(`Title too long`);
+  }
+
+  if (article.announce && article.announce.length < TextRestriction.shortMin) {
+    res
+      .status(HttpCode.BAD_REQUEST)
+      .send(`Announce too short`);
+  }
+
+  if (article.announce && article.announce.length < TextRestriction.shortMax) {
+    res
+      .status(HttpCode.BAD_REQUEST)
+      .send(`Announce too long`);
+  }
+
+  if (article.category && !article.category.length) {
+    res
+      .status(HttpCode.BAD_REQUEST)
+      .send(`Category is empty`);
+  }
+
+  if (article.fullText && article.fullText.length > TextRestriction.longMax) {
+    res
+      .status(HttpCode.BAD_REQUEST)
+      .send(`Publication is too long`);
   }
 
   next();
