19991be3801449fc8da2ddd56f7d3c177e82cf17
Alexey Ivanov <4985900@gmail.com>
2020-10-20 15:36:17 +0300

    feat: add pagination for /my page

--- src/frontend/routes/my-routes.js
+++ src/frontend/routes/my-routes.js
@@ -3,6 +3,7 @@
 const {Router} = require('express');
 const ArticleService = require('../data-service/article-service');
 const CommentService = require('../data-service/comment-service');
+const {MAX_ARTICLES_PER_PAGE} = require('../../constants');
 const {MAX_COMMENTS_PER_PAGE} = require('../../constants');
 const {getErrorTemplate} = require('../../utils/get-error-template');
 const {getLogger} = require('../../libs/logger');
@@ -12,11 +13,21 @@ const logger = getLogger();
 const myRoutes = new Router();
 
 myRoutes.get(`/`, async (req, res) => {
+  const {page = 1} = req.query;
+  const pageNumber = parseInt(page, 10);
+  const offset = pageNumber === 1 ? 0 : (pageNumber - 1) * MAX_ARTICLES_PER_PAGE;
+
   try {
-    const articles = await ArticleService.getAll();
+    const {articles, count} = await ArticleService.getAll(offset, MAX_ARTICLES_PER_PAGE);
+    const pagesCount = Math.ceil(count / MAX_ARTICLES_PER_PAGE);
 
     res.render(`my/my`, {
       articles,
+      pagesCount,
+      activePage: pageNumber,
+      prevIsActive: pageNumber !== 1,
+      nextIsActive: pageNumber < pagesCount,
+      myPath: '/my'
     });
   } catch (err) {
     logger.error(err);
@@ -24,6 +35,8 @@ myRoutes.get(`/`, async (req, res) => {
   }
 });
 
+// TODO: создание комментариев
+// TODO: удаление комментариев
 myRoutes.get(`/comments`, async (req, res) => {
   const {page = 1} = req.query;
   const pageNumber = parseInt(page, 10);
