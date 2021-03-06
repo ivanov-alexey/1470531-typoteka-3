b5e0e29a20b65744de4433e12ed89d4055cdcac4
Alexey Ivanov <4985900@gmail.com>
2021-03-06 08:31:48 +0300

    feat: show url:port on starting frontend server

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -5,7 +5,7 @@ const bodyParser = require(`body-parser`);
 const path = require(`path`);
 const helmet = require(`helmet`);
 const expressSession = require(`express-session`);
-const {DEFAULT_FRONT_PORT, PUBLIC_DIR, TEMPLATES_DIR} = require(`../constants`);
+const {DEFAULT_FRONT_PORT, PUBLIC_DIR, TEMPLATES_DIR, Message} = require(`../constants`);
 const mainRoute = require(`./routes/main-route`);
 const registerRoutes = require(`./routes/register-routes`);
 const loginRoutes = require(`./routes/login-routes`);
@@ -14,9 +14,11 @@ const categoriesRoutes = require(`./routes/categories-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const articlesRoutes = require(`./routes/articles-routes`);
 const logoutRoutes = require(`./routes/logout-routes`);
+const {getLogger} = require(`../libs/logger`);
 const {sessionStore} = require(`../configs/db-config`);
 const {secret} = require(`../configs/env-config`);
 
+const logger = getLogger();
 const app = express();
 
 app.set(`views`, path.resolve(__dirname, TEMPLATES_DIR));
@@ -49,4 +51,10 @@ app.use(`/categories`, categoriesRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articlesRoutes);
 
-app.listen(DEFAULT_FRONT_PORT);
+app.listen(DEFAULT_FRONT_PORT, (err) => {
+  if (err) {
+    logger.error(Message.serverStartError(DEFAULT_FRONT_PORT, err));
+  }
+
+  logger.info(Message.listenOnPort(DEFAULT_FRONT_PORT));
+});
