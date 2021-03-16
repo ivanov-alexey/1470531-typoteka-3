c6b33e8ab6ce662d69936cf947d0fd467d87d18a
Alexey Ivanov <4985900@gmail.com>
2021-03-16 16:00:30 +0300

    fix: fix path to files

--- src/backend/api/article/article.js
+++ src/backend/api/article/article.js
@@ -6,7 +6,7 @@ const newEntityValidator = require(`../../middlewares/new-entity-validator`);
 const articleExist = require(`../../middlewares/article-exists`);
 const articleSchema = require(`../../schemas/article`);
 const commentSchema = require(`../../schemas/comment`);
-const idValidator = require(`src/backend/middlewares/id-validator`);
+const idValidator = require(`../../middlewares/id-validator`);
 const {getLogger} = require(`../../../libs/logger`);
 
 const logger = getLogger();
--- src/backend/api/category/category.js
+++ src/backend/api/category/category.js
@@ -4,7 +4,7 @@ const {Router} = require(`express`);
 const {HttpCode} = require(`../../../constants`);
 const newEntityValidator = require(`../../middlewares/new-entity-validator`);
 const categorySchema = require(`../../schemas/category`);
-const idValidator = require(`src/backend/middlewares/id-validator`);
+const idValidator = require(`../../middlewares/id-validator`);
 const {getLogger} = require(`../../../libs/logger`);
 
 const logger = getLogger();
